aaa3c57f477c956172ea20bb664aef3c73c1b576
ofakduman <61577706+ofakduman@users.noreply.github.com>
2020-12-08 23:35:32 +0300

    Update test.py
    Added some unit test for user-login and arrange created before unit tests

--- FMAP/test.py
+++ FMAP/test.py
@@ -29,7 +29,7 @@ class FlaskTestCase(unittest.TestCase):
 	def test_myprofil(self):
 		tester = app.test_client(self)
 		response = tester.get('/myprofil', content_type = 'html/text')
-		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.status_code, 404)#not constructed page 
 
 	#For ensure that contactus page was load correctlly
 
@@ -51,6 +51,38 @@ class FlaskTestCase(unittest.TestCase):
 		tester = app.test_client(self)
 		response = tester.get('/signin', content_type = 'html/text')
 		self.assertEqual(response.status_code, 200)		
+
+	#For ensure that signin page was load correctlly
+
+	def test_addFootballArea(self):
+		tester = app.test_client(self)
+		response = tester.get('/addFootballArea', content_type = 'html/text')
+		self.assertEqual(response.status_code, 404)#not constructed page
+
+class requireUserLoginTest(unittest.TestCase):
+
+	# Ensure that addArea page requires user login -Now there is a problem because We already work this page-
+	def test_main_route_requires_addArea(self):
+		response = self.client.get('/addArea', follow_redirects=True)
+		self.assertIn(b'Please log in to access this page', response.data)
+
+	# Ensure that myprofil page requires user login -Now there is a problem because We already work this page-
+	def test_main_route_requires_myprofil(self):
+		response = self.client.get('/myprofil', follow_redirects=True)
+		self.assertIn(b'Please log in to access this page', response.data)
+
+	# Ensure that addFootballArea page requires user login 
+	def test_main_route_requires_addFootballArea(self):
+		response = self.client.get('/addFootballArea', follow_redirects=True)
+		self.assertIn(b'Please log in to access this page', response.data)
+
+
+class usetTest(unittest.TestCase):
+
+	# Databasede olan bir objenin siteye girip giremedigini kontrol eder
+	def test_posts_show_up_on_main_page(self):
+		response = self.client.post('/sigin',data=Users(name="admin", password="admin"),follow_redirects=True)
+		self.assertIn(b'This is a test. Only a test.', response.data)
 
 if __name__ == '__main__':
 	unittest.main()	
